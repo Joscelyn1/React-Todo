fce259d05eee84b22bec0ee2703ea02528183d34
Joscelyn <JoscelynRose@gmail.com>
2019-08-05 16:09:35 -0400

    added addItem and toggleItem functions

--- src/App.js
+++ src/App.js
@@ -41,6 +41,40 @@ class App extends React.Component {
     };
   }
 
+  toggleItem = id => {
+    // Update groceries on our state object
+    // use this.setState
+    // loop through the arr
+    // find which element we clicked update the "pruchased" property
+    this.setState({
+      groceries: this.state.toDos.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            // Same as:
+            // name: item.name,
+            // id: item.id,
+            // purchased: item.purchased,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
+  addItem = itemName => {
+    const newItem = {
+      name: itemName,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      toDoData: [...this.state.toDos, newItem]
+    });
+  };
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
